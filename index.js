17d9253f5fb116aea9b4cf5ac327307db38622b4
Matías Gaete <matias.gaete@smithcommerce.com>
2022-07-29 18:41:20 -0400

    added loop with timing

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import { findStock } from "./findStock.js";
 
-findStock()
+while (true) {
+  const time = new Date()
+  console.log(`Script ejecutado a las ${time.toLocaleTimeString()} el día ${time.toDateString()}`)
+  await findStock()
+  await sleep(900000) // 15 minutes
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
